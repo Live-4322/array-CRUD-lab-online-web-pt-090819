74704b29ea42588f1c48ad77dac76ae456910086
Charlie Oster <oster.charlie@yahoo.com>
2019-09-20 20:46:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,14 +8,10 @@ end
 
 def add_element_to_end_of_array(array, element)
   numbers << (5)
- 
- end
 end
 
 def add_element_to_start_of_array(array, element)
   numbers.unshift (0)
- 
- end
 end
 
 def remove_element_from_end_of_array(array)
